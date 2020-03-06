17f8f99515765497f49f69f2c7250811dbd65990
JialiangLiu <jl262@uw.edu>
2020-03-06 13:55:28 -0800

    add player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float velocity = 5;
+    public Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(velocity * Input.GetAxis("Horizontal"));
+        rb.AddForce(velocity * Input.GetAxis("Horizontal"),
+            0f,
+            velocity * Input.GetAxis("Vertical"));
     }
 }

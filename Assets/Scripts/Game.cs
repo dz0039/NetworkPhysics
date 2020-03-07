b13956a334b73cc356d7744a096827fb1645a872
JialiangLiu <jl262@uw.edu>
2020-03-06 16:50:03 -0800

    add some comments to make more readable

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -10,7 +10,8 @@ using UnityEngine.Assertions;
         15*15 cubes
         6 player
 */
-public class Game : MonoBehaviour {
+public class Game : MonoBehaviour
+{
     public GameObject cubePrefab;
     private RBObj[] _sceneCubes; // all scene cubes
     public GameObject playerPrefab;
@@ -22,8 +23,11 @@ public class Game : MonoBehaviour {
     private static Game _instance;
     public static Game Instance { get => _instance; }
 
-    void Start() {
-        if (_instance && _instance != this) {
+    void Start()
+    {
+        // instance should be null at first
+        if (_instance && _instance != this)
+        {
             Debug.LogError("Singleton Error");
             Destroy(gameObject);
             return;
@@ -49,54 +53,73 @@ public class Game : MonoBehaviour {
         ).SetActive(true);
     }
 
-    void Update() {
+    void Update()
+    {
         // hermit interpolation using current snapshot
 
     }
 
-    private static RBObj[] InitSceneCubes(GameObject prefab) {
+    // Initialize all the cubes on the plane with distance with each other
+    // and form a square.
+    // @param accepts the prefab of cube as the game object
+    // @returns an array that contains all the rigid body objects
+    private static RBObj[] InitSceneCubes(GameObject prefab)
+    {
         float bound = 8.0f;
-        float space = 1.6f;
+        float space = 1.6f; // the gap between each cube
         int n = 0;
         for (float i = -bound; i < bound; i += space) n++;
         n *= n;
         var res = new RBObj[n];
-        for (float i = -bound; i < bound; i += space) {
-            for (float j = -bound; j < bound; j += space) {
-                res[--n] = new RBObj {
-                Id = n,
-                Position = new Vector3(i, 3.0f, j),
-                Rotation = Quaternion.identity,
-                LVelocity = Vector3.zero,
-                AVelocity = Vector3.zero,
-                Go = Instantiate(prefab, new Vector3(i, 1.0f, j), Quaternion.identity)
+        for (float i = -bound; i < bound; i += space)
+        {
+            for (float j = -bound; j < bound; j += space)
+            {
+                res[--n] = new RBObj
+                {
+                    Id = n,
+                    Position = new Vector3(i, 3.0f, j),
+                    Rotation = Quaternion.identity,
+                    LVelocity = Vector3.zero,
+                    AVelocity = Vector3.zero,
+                    Go = Instantiate(prefab, new Vector3(i, 1.0f, j), Quaternion.identity)
                 };
             }
         }
         return res;
     }
 
-    private static RBObj[] InitPlayers(GameObject prefab) {
+    // Initialize the players on the plane.
+    // @param accepts the prefab of sphere as the game object
+    // @returns an array that contains all the rigid body objects
+    private static RBObj[] InitPlayers(GameObject prefab)
+    {
         int n = 6;
         var res = new RBObj[n];
-        for (int i = 0; i < n; i++) {
-            res[i] = new RBObj {
+        for (int i = 0; i < n; i++)
+        {
+            res[i] = new RBObj
+            {
                 Id = i,
-                    Position = Vector3.zero,
-                    Rotation = Quaternion.identity,
-                    LVelocity = Vector3.zero,
-                    AVelocity = Vector3.zero,
-                    Go = Instantiate(prefab, Vector3.zero, Quaternion.identity)
+                Position = Vector3.zero,
+                Rotation = Quaternion.identity,
+                LVelocity = Vector3.zero,
+                AVelocity = Vector3.zero,
+                Go = Instantiate(prefab, Vector3.zero, Quaternion.identity)
             }.SetActive(false);
         }
         return res;
     }
 
-    public Snapshot GetSnapshot() {
+    // Gets the snapshot of the current game scene
+    // @returns a Snapshot object
+    public Snapshot GetSnapshot()
+    {
         return null;
     }
 
-    public void AddSnapshot(Snapshot snapshot) {
+    public void AddSnapshot(Snapshot snapshot)
+    {
         // TODO:
         // for each cube?
         //   update status using one with highest priority if in (mySnap U newSnap)
@@ -109,24 +132,27 @@ public class Game : MonoBehaviour {
 
 }
 
-public class RBObj {
+public class RBObj
+{
     public int Id { get; set; }
     public Vector3 Position { get; set; }
     public Quaternion Rotation { get; set; }
-    public Vector3 LVelocity { get; set; }
-    public Vector3 AVelocity { get; set; }
+    public Vector3 LVelocity { get; set; }  // TODO: what velocity?
+    public Vector3 AVelocity { get; set; }  // angular velocity
     public GameObject Go { get; set; }
     public int Priority { get; set; } // only associate with main player
 
     private Rigidbody _rb = null;
 
-    public RBObj SetActive(bool val) {
+    public RBObj SetActive(bool val)
+    {
         // TODO: RB errors?
         Go.SetActive(val);
         return this;
     }
 
-    public RBObj SetRB(Vector3 pos, Quaternion rot, Vector3 lv, Vector3 av) {
+    public RBObj SetRB(Vector3 pos, Quaternion rot, Vector3 lv, Vector3 av)
+    {
         if (!_rb) _rb = Go.GetComponent<Rigidbody>();
         // TODO: hermit
         _rb.position = pos;
@@ -137,7 +163,8 @@ public class RBObj {
     }
 }
 
-public class Snapshot {
+public class Snapshot
+{
     public List<RBObj> cubeStates;
     public List<RBObj> playerStates;
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,7 +16,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(velocity * Input.GetAxis("Horizontal"));
         rb.AddForce(velocity * Input.GetAxis("Horizontal"),
             0f,
             velocity * Input.GetAxis("Vertical"));

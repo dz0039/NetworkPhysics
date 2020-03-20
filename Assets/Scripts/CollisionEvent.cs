34a71b0214f3b72f4006771b6b54f5d59828b45a
Tri0de <best108@uw.edu>
2020-03-20 10:59:22 -0700

    Added collision event

---
+++ Assets/Scripts/CollisionEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Equals("Cube_Physics(Clone)")) {
+            Debug.Log(collision.gameObject.name); // Give the cube higher priority.
+        }
+    }
+}
